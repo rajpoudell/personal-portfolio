76c3fc608b1bce8d8e0d73d6ed677bb8626b3438
Raj Poudel <raj.poudeel@gmail.com>
2023-08-10 00:30:38 +0545

    preloader updated

--- index.js
+++ index.js
@@ -1,27 +1,4 @@
 
-// const pic = document.querySelectorAll(".skillspic");
-// const skill = document.querySelectorAll(".aboutskills");
- 
-
-// for (let i = 0; i < pic.length; i++) {
-//     // pic[i].addEventListener("click", function() {
-//     //   pic[i].style.opacity = "0";
-//     //   skill[i].style.display = "block";
-      
-//     // });
-
-//     // for mouse over and mouse out 
-//     pic[i].addEventListener("mouseover", function() {
-//       pic[i].style.opacity = "0";
-//       skill[i].style.display = "block";
-      
-//     });
-//     pic[i].addEventListener("mouseout", function() {
-//       pic[i].style.opacity = "1";
-//       skill[i].style.display = "none";
-//     });
-//   }
-
 // js for disable right click of the mouse 
 
   document.addEventListener("contextmenu", function(event){
@@ -34,6 +11,16 @@ let preloader = document.querySelector('.preloader');
 window.addEventListener("load",function(){
   preloader.style.display = "none"
 })
+
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".loader");
+
+  loader.classList.add("loader--hidden");
+
+  loader.addEventListener("transitionend", () => {
+    document.body.removeChild(loader);
+  });
+});
 var typed = new Typed(".typing",{
   strings:["Developer ","UI/UX Designer ","Web Designer "],
   typeSpeed: 100,
