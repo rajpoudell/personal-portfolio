cfdaf89e69d108ed425b34a2d2840217f9ef3306
Raj Poudel <raj.poudeel@gmail.com>
2024-01-06 08:06:49 +0545

    loader is improve using error handling in js

--- index.js
+++ index.js
@@ -8,15 +8,39 @@
     // js for preloader 
 
 
-window.addEventListener("load", () => {
+// window.addEventListener("load", () => {
+//   const loader = document.querySelector(".loader");
+
+//   loader.classList.add("loader--hidden");
+
+//   loader.addEventListener("transitionend", () => {
+//     document.body.removeChild(loader);
+//   });
+// });
+
+
+// improve version of loader copied from chatgpt
+const onLoad = () => {
   const loader = document.querySelector(".loader");
 
-  loader.classList.add("loader--hidden");
+  if (loader) {
+    loader.classList.add("loader--hidden");
 
-  loader.addEventListener("transitionend", () => {
-    document.body.removeChild(loader);
-  });
-});
+    loader.addEventListener("transitionend", () => {
+      document.body.removeChild(loader);
+    });
+
+    // Remove the "load" event listener
+    window.removeEventListener("load", onLoad);
+  } else {
+    console.error("Loader element not found");
+  }
+};
+
+window.addEventListener("load", onLoad);
+
+
+
 var typed = new Typed(".typing",{
   strings:["Developer ","UI/UX Designer ","Web Designer "],
   typeSpeed: 100,
